f70072518d738c458d5d7e1d89f41b8c6bee1d0b
krzysztof.choma <>
2023-10-30 18:02:09 +0100

    Added timer function

--- src/app/game-header/timer/timer.component.ts
+++ src/app/game-header/timer/timer.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {interval, Subject, takeUntil, takeWhile, tap} from 'rxjs';
 
 @Component({
   selector: 'timer',
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.scss']
 })
-export class TimerComponent {
+export class TimerComponent implements OnDestroy {
+
+  time: number;
+  private destroy$ = new Subject<void>();
+
+  constructor() {
+    this.time = 10;
+    interval(1000).pipe(
+      takeWhile(()=> !!this.time),
+      tap(() => this.time--),
+      takeUntil(this.destroy$)
+    ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
 }
